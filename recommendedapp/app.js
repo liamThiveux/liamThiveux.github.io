328cd2b00238d0d07bd32e85593fe1fd4a1518b5
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-07 14:31:20 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -4,7 +4,7 @@ self.addEventListener('canmnakepayment', (evt) => {
 
 self.addEventListener('paymentrequest', (evt) => {
     evt.respondWith({
-        methodName: 'https://paylib.fr',
+        methodName: 'https://liamThiveux.github.io',
         details: {
             token: '1234567890',
         },
