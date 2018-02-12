70ad2d212eecf8f72e81c9c430181350f7bb1558
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-12 09:29:17 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -22,14 +22,26 @@ self.addEventListener('paymentrequest', (evt) => {
             compte: "Liam thiveux",
             cardNumber: "4200 XXXX XXXX XXXX",
             token: '1234567890',
-  
-        };
-      evt.openWindow("https://liamThiveux.github.io")
+    });
+});
+
+self.addEventListener('paymentrequest', function(e) {
+  e.respondWith(new Promise(function(resolve, reject) {
+    self.addEventListener('message', listener = function(e) {
+      self.removeEventListener('message', listener);
+      if (e.data.hasOwnProperty('name')) {
+        reject(e.data);
+      } else {
+        resolve(e.data);
+      }
+    });
+
+    e.openWindow("https://www.example.com/bobpay/pay")
     .then(function(windowClient) {
       windowClient.postMessage(e.data);
     })
     .catch(function(err) {
       reject(err);
     });
-    });
+  }));
 });
