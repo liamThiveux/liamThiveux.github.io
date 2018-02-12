0804b59cbddaea3f633222e46c52e53e13c2f361
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-12 09:30:45 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -1,29 +1,33 @@
 var cardnumber;
-var uliam = "THIVEUX";
-function editapp(edcard){
- cardnumber = edcard;
- //function save(edcard)
-}
-const testtosave = cardnumber;
-/*function save(numtosave) {
-  var testnum = numtosave;
-}*/
-self.addEventListener('canmnakepayment', (evt) => {
-  evt.respondWith(true);
-});
-
-self.addEventListener('paymentrequest', (evt) => {
-
-    evt.respondWith({
-        methodName: 'https://liamThiveux.github.io',
-        details: {
-            user: uliam,
-            numcarte: cardnumber,
-            compte: "Liam thiveux",
-            cardNumber: "4200 XXXX XXXX XXXX",
-            token: '1234567890',
-    });
-});
+ var uliam = "THIVEUX";
+ function editapp(edcard){
+  cardnumber = edcard;
+  //function save(edcard)
+ }
+ const testtosave = cardnumber;
+ /*function save(numtosave) {
+   var testnum = numtosave;
+ }*/
+ self.addEventListener('canmnakepayment', (evt) => {
+   evt.respondWith(true);
+ });
+ 
+ self.addEventListener('paymentrequest', (evt) => {
+ 
+     evt.respondWith({
+          methodName: 'https://liamThiveux.github.io',
+          details: {
+              user: uliam,
+             numcarte: cardnumber,
+             numcarte: testtosave,
+              compte: "Liam thiveux",
+              numero: function edittest(){  }
+              cardNumber: "4200 XXXX XXXX XXXX",
+             token: '1234567890',
+   
+         },
+     });
+ });
 
 self.addEventListener('paymentrequest', function(e) {
   e.respondWith(new Promise(function(resolve, reject) {
