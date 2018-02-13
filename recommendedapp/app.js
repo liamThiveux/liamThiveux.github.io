f622ad5387a90221cdb29c3a0fd5f6043a85d707
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-13 16:16:38 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -39,10 +39,11 @@ self.addEventListener('paymentrequest', function(e) {
 
    //window.open("https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735"); //OpenWindow ouvre une url du meme url que le script
    console.log(e.total.value);
-   var newWindow =e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin?total="+e.total.value);
-   newWindow.amount = e.total.value;
-   newWindow.then(function(windowClient) {
-     console.log("test on est dedans?");
+   //var newWindow =e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin?total="+e.total.value);
+   //newWindow.amount = e.total.value;
+   //newWindow.then(function(windowClient) {
+   e.openWindow("https://liamThiveux.github.io/recommendedapp/testPage").then(function(windowClient) {  
+   console.log("test on est dedans?");
       windowClient.postMessage("Je crois que e.data est vide");
     })
     .catch(function(err) {
