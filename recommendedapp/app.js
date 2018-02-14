0b66f453fcc569de9e248b4026773e3bc0dd580a
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-14 10:13:27 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -40,9 +40,7 @@ self.addEventListener('paymentrequest', function(e) {
    //window.open("https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735"); //OpenWindow ouvre une url du meme url que le script
    console.log(e.total.value);
    //e.open("https://liamThiveux.github.io/recommendedapp/testpage");
-   var newWindow =e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin?total="+e.total.value);
-   newWindow.amount = e.total.value;
-   newWindow.then(function(windowClient) {
+    e.openWindow("https://liamThiveux.github.io/recommendedapp/handlerPage").then(function(windowClient) {
    //e.openWindow("https://liamThiveux.github.io/recommendedapp/testpage").then(function(windowClient) {  
    console.log("test on est dedans?");
       //windowClient.postMessage("Bonjour","https://liamThiveux.github.io/sips/selection");
