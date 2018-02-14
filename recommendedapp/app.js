1dda90458718f65a4bd500e85640373148de13eb
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-14 13:07:44 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -66,7 +66,7 @@ self.addEventListener('paymentrequest', function(e) {
 
     e.openWindow("https://liamThiveux.github.io/recommendedapp/testpage")
     .then(function(windowClient) {
-      windowClient.postMessage(e.data);
+      windowClient.postMessage(e.data,"https://liamThiveux.github.io");
     })
     .catch(function(err) {
       reject(err);
