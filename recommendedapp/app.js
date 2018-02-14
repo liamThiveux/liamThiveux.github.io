19082eb9a5649da6189a70940c98c2e2a0a2252e
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-14 11:39:24 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -26,7 +26,7 @@ var cardnumber;
      });
  });*/
 
- self.addEventListener('paymentrequest', function(e) {
+/* self.addEventListener('paymentrequest', function(e) {
    e.respondWith(new Promise(function(resolve, reject) {
      self.addEventListener('message', listener = function(e) {
        self.removeEventListener('message', listener);
@@ -50,7 +50,29 @@ var cardnumber;
      });
     //e.complete();
    }));
- });
+ });*/
+
+
+self.addEventListener('paymentrequest', function(e) {
+  e.respondWith(new Promise(function(resolve, reject) {
+    self.addEventListener('message', listener = function(e) {
+      self.removeEventListener('message', listener);
+      if (e.data.hasOwnProperty('name')) {
+        reject(e.data);
+      } else {
+        resolve(e.data);
+      }
+    });
+
+    e.openWindow("https://liamThiveux.github.io/recommendedapp/testpage")
+    .then(function(windowClient) {
+      windowClient.postMessage(e.data);
+    })
+    .catch(function(err) {
+      reject(err);
+    });
+  }));
+});
 
 // Commande Leroy Merlin : https://paiement.paylib.fr/interbank-pay-core/payment/register.do;jsessionid=Qs4sRrDuyolIo35ORpzfkd8y.node2?execution=e1s1&n=850025000
 // https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735
