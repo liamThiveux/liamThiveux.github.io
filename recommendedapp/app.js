589ee721a3332a25d09268d519049d39fc1c1015
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-14 11:11:39 +0100

    Update app.js

--- recommendedapp/app.js
+++ recommendedapp/app.js
@@ -26,38 +26,31 @@ var cardnumber;
      });
  });*/
 
-self.addEventListener('paymentrequest', function(e) {
-  e.respondWith(new Promise(function(resolve, reject) {
-    self.addEventListener('message', listener = function(e) {
-      self.removeEventListener('message', listener);
-      if (e.data.hasOwnProperty('name')) {
-        reject(e.data);
-      } else {
-        resolve(e.data);
-      }
-    });
-
-   //window.open("https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735"); //OpenWindow ouvre une url du meme url que le script
-   console.log(e.total.value);
-   //e.open("https://liamThiveux.github.io/recommendedapp/testpage");
-    /*var newWindow =e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin?total="+e.total.value);
-    newWindow.amount = e.total.value;
-    newWindow.then(function(windowClient) {*/
-    e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin").then(function(windowClient) {
-   console.log("test on est dedans?");
-    /* if (!windowClient.visibilityState) {
-      console.log("fermée!");
-      e.complete();
-     }*/
-         windowClient.postMessage("Bonjour");
-    })
-    .catch(function(err) {
-      reject(err);
-    });
-   //e.complete();
-  }));
- //setTimeout(function(){ e.complete() }, 6000);
-});
+ self.addEventListener('paymentrequest', function(e) {
+   e.respondWith(new Promise(function(resolve, reject) {
+     self.addEventListener('message', listener = function(e) {
+       self.removeEventListener('message', listener);
+       if (e.data.hasOwnProperty('name')) {
+         reject(e.data);
+       } else {
+         resolve(e.data);
+       }
+     });
+ 
+     //window.open("https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735"); //OpenWindow ouvre une url du meme url que le script
+     console.log(e.total.value);
+     //e.open("https://liamThiveux.github.io/recommendedapp/testpage");
+     e.openWindow("https://liamThiveux.github.io/recommendedapp/paylibLogin").then(function(windowClient) {
+     //e.openWindow("https://liamThiveux.github.io/recommendedapp/testpage").then(function(windowClient) {  
+     console.log("test on est dedans?");
+      windowClient.postMessage("Bonjour","https://liamThiveux.github.io/sips/selection");
+     })
+     .catch(function(err) {
+       reject(err);
+     });
+    //e.complete();
+   }));
+ });
 
 // Commande Leroy Merlin : https://paiement.paylib.fr/interbank-pay-core/payment/register.do;jsessionid=Qs4sRrDuyolIo35ORpzfkd8y.node2?execution=e1s1&n=850025000
 // https://paiement.paylib.fr/interbank-pay-core/payment/register.do?execution=e2s1&n=1841686735
