fb384cea0a1f62c6052c5a4e865bc66117621f23
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-02-21 09:45:42 +0100

    Update w3CPayment.js

--- sips/w3CPayment.js
+++ sips/w3CPayment.js
@@ -7,8 +7,8 @@ function initPaymentRequest(networks) {
 //  let networks = ['amex', 'diners', 'discover', 'jcb', 'mastercard', 'unionpay',
       //'visa', 'mir'];
   let types = ['debit', 'credit', 'prepaid'];
-  var longdate = "automaticResponseURL=https://sims.ibt-agile.qlf.sips.priv.atos.fr/webservices/PaymentConfirmationCNRT/targetPaymentConfirmationAutoCNRT.jsp|normalReturnURL=https://sims.ibt-agile.qlf.sips.priv.atos.fr/webservices/PaymentConfirmation/PaymentConfirmation.jsp|merchantId=233118849141631|amount=1000|currencyCode=978|transactionReference=1519202085478|keyVersion=1|transactionOrigin=SIMS|automaticErrorResponseInitPOST=https://sims.ibt-agile.qlf.sips.priv.atos.fr/SipsAutomaticErrorInitResponseHandler|manualErrorResponseInitPOST=https://sims.ibt-agile.qlf.sips.priv.atos.fr/SipsManualResponseHandler"
-  var seal = "abd5643676938ae62ac9578993e834224a1d1c0204bb0a0abf5dd921e3b21edc";
+  var longdata = "automaticResponseURL=https://sims.ibt-agile.qlf.sips.priv.atos.fr/webservices/PaymentConfirmationCNRT/targetPaymentConfirmationAutoCNRT.jsp|normalReturnURL=https://sims.ibt-agile.qlf.sips.priv.atos.fr/webservices/PaymentConfirmation/PaymentConfirmation.jsp|merchantId=233118849141631|amount=1000|currencyCode=978|transactionReference=1519202687808|keyVersion=1|transactionOrigin=SIMS|automaticErrorResponseInitPOST=https://sims.ibt-agile.qlf.sips.priv.atos.fr/SipsAutomaticErrorInitResponseHandler|manualErrorResponseInitPOST=https://sims.ibt-agile.qlf.sips.priv.atos.fr/SipsManualResponseHandler"
+  var seal = "e184cf6d1c478c99bd0e91f113ac3e02cd8bced9ddb0bd3bef5781af2b06d52a";
   let supportedInstruments = [
 	{
 		supportedMethods: 'basic-card',
