8d2cc8d2af9563e18ed230bec6134de6c52318b7
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-03-28 10:11:24 +0200

    Update basic-card.js

--- basic-card-app/basic-card.js
+++ basic-card-app/basic-card.js
@@ -14,7 +14,7 @@
      });
     var cardNumber = "5134 1012 3456 7890";
     var expDate = "00/00";
-     e.openWindow("https://liamThiveux.github.io/basic-card-app/CVC.html").then(function(windowClient) {
+     e.openWindow("https://liamThiveux.github.io/basic-card-app/bank1-login.html").then(function(windowClient) {
    windowClient.postMessage({data:e.methodData[0].data, cbNumber: cardNumber, date: expDate});
      })
      .catch(function(err) {
