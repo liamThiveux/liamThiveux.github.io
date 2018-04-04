4b85003bb9c3621d1ffc0a1cc043fd23912d6cb9
liamThiveux <32488971+liamThiveux@users.noreply.github.com>
2018-04-04 17:34:20 +0200

    Create basic-card2.js

---
+++ basic-card-app/basic-card2.js
@@ -0,0 +1,24 @@
+ self.addEventListener('canmnakepayment', (evt) => {
+   evt.respondWith(true);
+ });
+
+ self.addEventListener('paymentrequest', function(e) {
+  e.respondWith(new Promise(function(resolve, reject) {
+     self.addEventListener('message', listener = function(e) {
+       self.removeEventListener('message', listener);
+       if (e.data.hasOwnProperty('name')) {
+         reject(e.data);
+       } else {
+         resolve(e.data);
+       }
+     });
+    var cardNumber = "1111 1012 3456 7890";
+    var expDate = "00/00";
+     e.openWindow("https://liamThiveux.github.io/basic-card-app/CVC.html").then(function(windowClient) {
+   windowClient.postMessage({data:e.methodData[0].data, cbNumber: cardNumber, date: expDate});
+     })
+     .catch(function(err) {
+       reject(err);
+     });
+   }));
+ });
